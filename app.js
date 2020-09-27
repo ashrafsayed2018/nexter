9c065d0ac1daf751022ad699e7969e4da743b21b
unknown <ashrafsayed19841111@gmail.com>
2020-09-27 03:09:59 +0200

    some modifications

--- app.js
+++ app.js
@@ -17,18 +17,12 @@ if('serviceWorker' in navigator) {
 
       let deferredPrompt;
       const addBtn = document.querySelector('.add-button');
-      addBtn.style.display = 'none';
 
       window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault();
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
-        // Update UI to notify the user they can add to home screen
-        if(window.innerWidth > 1200) {
-            addBtn.style.display = 'block';
-
-        }
       
         addBtn.addEventListener('click', (e) => {
           // hide our user interface that shows our A2HS button
